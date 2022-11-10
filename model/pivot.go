0d604473157cb38bb9ef3bac517acea09a916277
arifwidiasan <arifw.subagio17@gmail.com>
2022-11-10 13:32:38 +0700

    edit pivot division field

--- model/pivot.go
+++ model/pivot.go
@@ -4,7 +4,6 @@ import "time"
 
 type Pivot_division_field struct {
 	ID            uint        `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name          string      `gorm:"unique;not null" json:"name"`
 	CreatedAt     time.Time   `json:"created_at"`
 	DivisionID    uint        `json:"division_id"`
 	Division      Division    `json:"division" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
